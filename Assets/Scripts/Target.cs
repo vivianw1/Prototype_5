1ea1f06321385ed6fd66c2c68c11711f2f366ce3
vivianw1 <viwu1@s.sfusd.edu>
2023-04-04 10:16:29 -0700

    Replace messy code with new methods

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,16 +5,44 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private float minSpeed = 12; 
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb= GetComponent<Rigidbody>();
 
-        targetRb.AddForce(Vector3.up * Random.Range(12,16), ForceMode.Impulse);
-        targetRb.AddTorque(Random.Range(-10,10), Random.Range(-10,10), Random.Range(-10,10),ForceMode.Impulse);
+        targetRb.AddForce(Vector3.up * Random.Range(minSpeed,maxSpeed), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-maxTorque,maxTorque), Random.Range(-maxTorque,maxTorque), Random.Range(-maxTorque,maxTorque),ForceMode.Impulse);
     
-        transform.position = new Vector3(Random.Range(-4,4),-6);
+        transform.position = new Vector3(Random.Range(-xRange,xRange),ySpawnPos);
         
     }
+}
+
+// Update is called once per frame
+void Update()
+{
+
+}
+
+Vector3 RandomForce()
+{
+    return Vector3.up * Random.Range(minSpeed,maxSpeed);
+}
+
+float RandomTorque()
+{
+    return Random.Range(-maxTorque,maxTorque)
+}
+
+Vector3 RandomSpawnPos()
+{
+    return new Vector3(Random.Range (-xRange,xRange)ySpawnPos);
 }
